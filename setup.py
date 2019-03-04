d98d5a758013117c422baf276532c7c86518dd85
Anthony Horton <anthony.horton@drhotdog.net>
2019-03-05 02:16:53 +1100

    simple website

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md",  "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pisces",
+    version="0.0.1",
+    author="Anthony Horton",
+    author_email="anthony.horton@drhotdog.net",
+    description="Pisces aquarium control system for Raspberry Pi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnthonyHorton/pisces",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licence :: OSI Approved :: BSD 3 Clause",
+        "Operating System :: Raspbian",
+    ],
+)
