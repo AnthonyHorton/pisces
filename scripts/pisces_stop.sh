2c15ecad996fb99b756da85feab318ee3dea0991
Anthony Horton <anthony.horton@aao.gov.au>
2019-11-19 01:43:25 +1100

    Kill, kill, kill

--- scripts/pisces_stop.sh
+++ scripts/pisces_stop.sh
@@ -2,8 +2,10 @@
 byobu-tmux switch-client -t pisces # Make sure we're sending shutdown commands to the right session
 
 byobu-tmux send-keys -t 2 -l 'pisces.stop_all()' # Shutdown pisces
-byobu-tmux send-keys 'C-m'
+byobu-tmux send-keys -t 2 'C-m'
 sleep 10s # Give it time
-byobu-tmux send-keys 'C-d' 'C-m' # Exit console
+byobu-tmux send-keys -t 2 'C-d'
+sleep 5s
+byobu-tmux send-keys -t 2 'C-m' # Exit console
 
 byobu kill-session -t pisces
