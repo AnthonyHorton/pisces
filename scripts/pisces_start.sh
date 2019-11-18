8f7967f3ac7fa1bc75b00c2dbb0a1f62c5a41688
Anthony Horton <anthony.horton@aao.gov.au>
2019-11-19 01:07:26 +1100

    Update startup/shutdown scripts.

--- scripts/pisces_start.sh
+++ scripts/pisces_start.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 byobu-tmux new-session -d -s pisces -n pisces # Create a new detached Byobu session called pisces
-byobu-tmux split-window
+byobu-tmux split-window -v -p 67
+byobu-tmux split-window -v -p 33
 
 byobu-tmux send-keys -t 0 'cd $PISCES/logs' 'C-m'
 byobu-tmux send-keys -t 0 'grc tail -n 1000 -F pisces.log' 'C-m' # Start watching the log
 
-byobu-tmux send-keys -t 1 'cd $PISCES' 'C-m'
-byobu-tmux send-keys -t 1 -R 'ipython' 'C-m'
+byobu-tmux send-keys -t 0 'cd $PISCES/data' 'C-m'
+byobu-tmux send-keys -t 0 'tail -n 1000 -F pisces.dat' 'C-m' # Start watching the data
+
+byobu-tmux send-keys -t 2 'cd $PISCES' 'C-m'
+byobu-tmux send-keys -t 2 -R 'ipython' 'C-m'
 sleep 10s # Need to wait a while for the console to start, otherwise console commands get lost
-byobu-tmux send-keys -t 1 'from pisces.core import Pisces' 'C-m'
+byobu-tmux send-keys -t 2 'from pisces.core import Pisces' 'C-m'
 sleep 3s # Waiting for the import prevents formatting getting messed up
-byobu-tmux send-keys -t 1 -l 'pisces=Pisces()' # Literal flag needed for this one
-byobu-tmux send-keys -t 1 'C-m' # Start pisces
+byobu-tmux send-keys -t 2 -l 'pisces = Pisces()' # Literal flag needed for this one
+byobu-tmux send-keys -t 2 'C-m' # Start pisces
